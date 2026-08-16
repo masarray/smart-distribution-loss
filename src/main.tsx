780f473f0ed9c0114b1e4e08d89ddfc65983bc54
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-16 19:18:57 +0700

    P5: load progress motion layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { FieldOperationalCockpitP5 } from "@/components/sdl/FieldOperationalCockpitP5";
 import "./styles.css";
+import "./progress-motion.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
