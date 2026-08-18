9a2f69995de035df40c517443fb02fde7f66d926
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-18 17:55:44 +0700

    P15: wire language provider into app shell

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AboutSheet } from "@/components/sdl/AboutSheet";
 import { FieldOperationalCockpitP5 } from "@/components/sdl/FieldOperationalCockpitP5";
+import { LanguageControl } from "@/components/sdl/LanguageControl";
+import { LanguageProvider } from "@/lib/sdl/i18n";
 import "./styles.css";
 import "./progress-motion.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <FieldOperationalCockpitP5 />
-    <AboutSheet />
+    <LanguageProvider>
+      <App />
+      <FieldOperationalCockpitP5 />
+      <AboutSheet />
+      <LanguageControl />
+    </LanguageProvider>
   </React.StrictMode>,
 );
